da7446f2c593637b0321ce125e94695328ce22af
PKiran914514 <132054239+PKiran914514@users.noreply.github.com>
2026-09-17 03:00:33 +0530

    uplading on ofc nw

--- myproject.spec.ts
+++ myproject.spec.ts
@@ -1,16 +1,25 @@
 import { test, expect } from "@playwright/test";
+import { await , fill } from "module";
 
 
 
 
-
-test("test name",async({page})=>{
+test("log_in",async({page})=>{
 
     await page.goto("https://demowebshop.tricentis.com/");
-    
-    
-    
-    await page.goto(``);
 
+    await page.locator(`.ico-login`).click();
+    await page.locator(`#Email`).fill(`kbp123@gmail.com`);
+    await page.locator(`Password`).fill(`Manufacturers`);
+    await page.locator(`.button-1.login-button`).click();
+
+})
+
+test("log_in",async({page})=>{
+
+
+    await 
+
+    
 
 })
--- playwrite_prerqusite.spec.ts
+++ playwrite_prerqusite.spec.ts
@@ -6,5 +6,7 @@ import { test, expect } from "@playwright/test";
 
 test("test name",async({page})=>{
 
-    await page.goto("//url")})
-kkkkk
+    await page.goto("//url")
+
+
+})

17d0aed21073d5d926f41f1e54b90ce45b2dd8b1
PKiran914514 <132054239+PKiran914514@users.noreply.github.com>
2026-09-17 01:45:56 +0530

    jei

--- myproject.spec.ts
+++ myproject.spec.ts
@@ -7,6 +7,32 @@ import { test, expect } from "@playwright/test";
 test("test name",async({page})=>{
 
     await page.goto("https://demowebshop.tricentis.com/");
+    
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    // https://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshedehttps://vscode.dev/github/PKiran914514/refresheshede
+    
+    
+    
     await page.goto(``);
 
 

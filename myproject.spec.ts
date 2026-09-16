7e8f84eb1df97ad3b04d74e5e8ae5ae9cd723f9a
PKiran914514 <132054239+PKiran914514@users.noreply.github.com>
2026-09-17 01:01:23 +0530

    hii

---
+++ myproject.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+
+
+
+
+
+test("test name",async({page})=>{
+
+    await page.goto("https://demowebshop.tricentis.com/");
+    await page.goto(``);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+/////code ends here 
+})

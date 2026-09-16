96ec1a76d1a6524caea51ade82a32e9c4839c9a9
PKiran914514 <132054239+PKiran914514@users.noreply.github.com>
2026-09-17 04:38:43 +0530

    hii

--- myproject.spec.ts
+++ myproject.spec.ts
@@ -15,10 +15,38 @@ test("log_in",async({page})=>{
 
 })
 
+
+
+
 test("log_in",async({page})=>{
 
+    // let catagaris =page.locator(`//ul[@class='top-menu']//a`)  //inside a link and text preset
+    // let cards = page.locator(`.item-box`)                      // 
+    
+    
 
-    await 
+    let tital_of_books_from_database =["Computing and Internet","Copy of Computing and Internet EX" ,"Fiction", "Fiction EX" , "Health Book" , "Science"];
+
+
+    let books_catagary = page.locator(`//ul[@class='top-menu']//a[@href='/books']`);
+    await page.goto(books_catagary).click();
+
+    let card_tital =page.locator(`//div[@class="item-box"]/descendant::h2[@class='product-title']//a`); //inside a link and text preset
+
+    tital_of_books_from_database.forEach(element => {
+        expect(await card_tital.alltextcontent).toContain(element);
+        console.log(element+` is present in `+ card_tital.alltextcontent);
+        
+        
+    });
+
+
+
+    await expect(card_tital).havetext();
+
+
+
+
 
     
 
